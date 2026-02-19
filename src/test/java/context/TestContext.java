4931081f7d784055d57d7354cbf31f337ecd66a2
jreysanmig <johnreysanmiguel@gmail.com>
2026-02-20 02:30:22 +1100

    Added picocontainer and TestContext

---
+++ src/test/java/context/TestContext.java
@@ -0,0 +1,11 @@
+package context;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TestContext {
+    public RequestSpecification requestSpec;
+    public Response response;
+
+
+}
--- src/test/java/stepDefinitions/AuthorSteps.java
+++ src/test/java/stepDefinitions/AuthorSteps.java
@@ -1,35 +1,50 @@
 package stepDefinitions;
 
+import context.TestContext;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.*;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import io.restassured.builder.RequestSpecBuilder;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class AuthorSteps {
 
-    static Response response;
+    private Scenario scenario;
+    private final TestContext context;
+
+    public AuthorSteps(TestContext context) {
+        this.context = context;
+    }
+
+    @Before
+    public void beforeHook(Scenario scenario) {
+        this.scenario = scenario;
+        this.context.requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://openlibrary.org/authors/OL1A.json")
+                .build();
+    }
 
     @Given("I call the Open Library author API")
     public void i_call_the_open_library_author_api() {
-        response =
-            given().
-            when().
-                get("https://openlibrary.org/authors/OL1A.json");
+        context.response =
+                given(context.requestSpec).
+                when().get();
+        context.requestSpec.log().all();
     }
     @Then("the response status should be {int}")
     public void the_response_status_should_be(int statusCode) {
-        response.then().assertThat().statusCode(statusCode);
+        context.response.then().assertThat().statusCode(statusCode);
+        scenario.attach(context.response.body().asPrettyString(), "application/json", "Response");
     }
     @Then("the personal_name should be {string}")
     public void the_personal_name_should_be(String personalName) {
-        response.then().body("personal_name", equalTo(personalName));
+        context.response.then().body("personal_name", equalTo(personalName));
     }
 
     @Then("the alternate_names should include {string}")
     public void the_alternate_names_should_include(String alternateName) {
-        response.then().body("alternate_names", hasItem(alternateName));
+        context.response.then().body("alternate_names", hasItem(alternateName));
     }
 }
